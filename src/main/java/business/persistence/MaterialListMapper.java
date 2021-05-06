875a054cb5addd234129fb7360c635cf332e98cd
cph-sn286 <73526072+cph-sn286@users.noreply.github.com>
2021-05-06 12:21:12 +0200

    materialmapper

--- src/main/java/business/persistence/MaterialListMapper.java
+++ src/main/java/business/persistence/MaterialListMapper.java
@@ -1,4 +1,44 @@
 package business.persistence;
 
+import business.entities.MaterialsList;
+import business.exceptions.UserException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaterialListMapper {
+
+    //todo insertmateriallist
+    Database database;
+    public List<MaterialsList> getMaterialListByCarportId(int carportId) throws UserException {
+        List<MaterialsList> materialsListsCarport = new ArrayList<>();
+        try (Connection connection = database.connect()) {
+            String sql = "SELECT * FROM orders WHERE user_id = ?";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                ps.setInt(1, carportId);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+
+                    int materialList_id = rs.getInt("materialList_id");
+                    int carport_id = rs.getInt("carport_id");
+                    int materials_id = rs.getInt("materials_id");
+                    double amount = rs.getDouble("amount");
+                    int unit_id = rs.getInt("unit_id");
+                    materialsListsCarport.add(new MaterialsList(materialList_id, carport_id, materials_id, amount, unit_id));
+                }
+                return materialsListsCarport;
+
+            } catch (SQLException ex) {
+                throw new UserException("der findes ingen material liste for carport = " + carportId);
+//                throw new UserException(ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            throw new UserException("Connection to database could not be established");
+        }
+    }
+
+
 }
--- src/main/java/business/persistence/MaterialsMapper.java
+++ src/main/java/business/persistence/MaterialsMapper.java
@@ -1,18 +1,15 @@
 package business.persistence;
 
+import business.entities.Carport;
 import business.entities.Materials;
 import business.entities.User;
 import business.exceptions.UserException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MaterialsMapper {
-    // todo material mapper + sizes mapper i denne klasse.
 
     Database database;
 
@@ -68,6 +65,96 @@ public class MaterialsMapper {
         } catch (SQLException ex) {
             throw new UserException("Connection to database could not be established");
         }
+    }
+
+    public int deleteMaterial(int material_id) throws UserException {
+        int rowaAffected = 0;
+        try (Connection connection = database.connect()) {
+            String sql = "DELETE FROM materials " +
+                    "WHERE materials_id = ?";
+
+            while (rowaAffected == 0) {
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setInt(1, material_id);
+                    rowaAffected = ps.executeUpdate();
+
+                    if (rowaAffected == 0) {
+                        rowaAffected = 1;
+                    }
+                } catch (SQLException ex) {
+                    throw new UserException(ex.getMessage());
+                }
+            }
+            while (rowaAffected >= 1) {
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setInt(1, material_id);
+                    rowaAffected += ps.executeUpdate();
+                    break;
+                } catch (SQLException ex) {
+                    throw new UserException(ex.getMessage());
+                }
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return rowaAffected;
+    }
+
+    public int updateMaterial(Double price, String description, int materials_id) throws UserException {
+        try (Connection connection = database.connect()) {
+            String sql = "UPDATE materials SET price = ?, description = ? WHERE materials_id = ?";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                ps.setDouble(1, price);
+                ps.setString(2, description);
+                ps.setInt(3,materials_id);
+                int rowsInserted = ps.executeUpdate();
+                return rowsInserted;
+
+            } catch (SQLException ex) {
+                throw new UserException(ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            throw new UserException("Connection to database could not be established");
+        }
+    }
+
+    public Materials insertMaterial(Materials materials) throws UserException {
+        boolean result = false;
+        int newId = 0;
+        String sql = "insert into materials (materials_id, name, sizes_id, description, price) values (?,?,?,?,?)";
+        try (Connection connection = database.connect()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, materials.getMaterials_id());
+                ps.setString(2, materials.getName());
+                ps.setInt(3, materials.getSizes_id());
+                ps.setString(4, materials.getDescription());
+                ps.setDouble(5,materials.getPrice());
+
+
+                int rowsAffected = ps.executeUpdate();
+                if (rowsAffected == 1) {
+                    result = true;
+                }
+                ResultSet idResultset = ps.getGeneratedKeys();
+                if (idResultset.next()) {
+                    newId = idResultset.getInt(1);
+                    materials.setMaterials_id(newId);
+                } else {
+                    materials = null;
+                }
+            } catch (SQLException ex) {
+
+                throw new UserException(ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            throw new UserException("connection to database could not be established");
+        }
+
+
+        return materials;
     }
 
 
