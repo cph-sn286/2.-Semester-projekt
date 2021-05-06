9f1a7407baf7399b7c709be7b995eca7e0fbb8f4
cph-sn286 <73526072+cph-sn286@users.noreply.github.com>
2021-05-06 12:27:53 +0200

    materiallistmapper

--- src/main/java/business/persistence/MaterialListMapper.java
+++ src/main/java/business/persistence/MaterialListMapper.java
@@ -22,12 +22,12 @@ public class MaterialListMapper {
                 ResultSet rs = ps.executeQuery();
                 while (rs.next()) {
 
-                    int materialList_id = rs.getInt("materialList_id");
+                    int materiallist_id = rs.getInt("materiallist_id");
                     int carport_id = rs.getInt("carport_id");
                     int materials_id = rs.getInt("materials_id");
                     double amount = rs.getDouble("amount");
                     int unit_id = rs.getInt("unit_id");
-                    materialsListsCarport.add(new MaterialsList(materialList_id, carport_id, materials_id, amount, unit_id));
+                    materialsListsCarport.add(new MaterialsList(materiallist_id, carport_id, materials_id, amount, unit_id));
                 }
                 return materialsListsCarport;
 
@@ -40,5 +40,39 @@ public class MaterialListMapper {
         }
     }
 
+    public MaterialsList insertMaterialList(MaterialsList materialsList) throws UserException {
+        boolean result = false;
+        int newId = 0;
+        String sql = "insert into materialList (materiallist_id, carport_id, materials_id, amount, unit_id) values (?,?,?,?,?)";
+        try (Connection connection = database.connect()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, materialsList.getMaterialList_id());
+                ps.setInt(2, materialsList.getCarport_id());
+                ps.setInt(3, materialsList.getMaterials_id());
+                ps.setDouble(4,materialsList.getAmount());
+                ps.setInt(5,materialsList.getUnit_id());
+
+                int rowsAffected = ps.executeUpdate();
+                if (rowsAffected == 1) {
+                    result = true;
+                }
+                ResultSet idResultset = ps.getGeneratedKeys();
+                if (idResultset.next()) {
+                    newId = idResultset.getInt(1);
+                    materialsList.setMaterialList_id(newId);
+                } else {
+                    materialsList = null;
+                }
+            } catch (SQLException ex) {
+
+                throw new UserException(ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            throw new UserException("connection to database could not be established");
+        }
+
+
+        return materialsList;
+    }
 
 }
