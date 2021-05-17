260f596f3fe6e930c21909e14fb014aaa640372c
AndersLind99 <cph-al350@cphbusiness.dk>
2021-05-17 10:40:59 +0200

    basic postCalc finished

--- src/main/java/business/services/carportCalc.java
+++ src/main/java/business/services/carportCalc.java
@@ -5,18 +5,38 @@ import business.entities.Result;
 public class carportCalc {
 
 
-   protected Result calcPost(){
-       Result result = new Result();
+    protected Result calcPost(double length, double width) {
+        Result result = new Result();
+
+        length = 600;
+        width = 780;
+
+        int postAmount = 4;
+
+        if (length > 300) {
+
+            while (length > 300) {
+
+                length = -300;
+                postAmount = +2;
 
 
+            }
+
+            result = new Result(300, postAmount);
 
 
+        }
+
+        if (length < 300 || length == 300) {
+
+            result = new Result(300, postAmount);
+            return result;
+        }
 
 
-       return result;
-   }
-
-
+        return result;
+    }
 
 
 }
